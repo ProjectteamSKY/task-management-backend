e1fb4b6b320206173276b724b3f647a12e2bf8b2
Keerthanaraja7312 <rkeerthana492@gmail.com>
2026-03-26 12:04:37 +0530

    integrate slack sdk

--- app/domain/task_domain.py
+++ app/domain/task_domain.py
@@ -1,8 +1,13 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class TaskCreate(BaseModel):
-    id: str
     title: str
-    description: str
-    duration_units: int
+    description: Optional[str] = None
     status: str
+    priority: str
+    estimated_hours: int
+    assigned_to: Optional[int] = None
+    project_name: str
+    start_date: Optional[str] = None   # "YYYY-MM-DD"
+    end_date: Optional[str] = None     # "YYYY-MM-DD"
--- app/routers/task_router.py
+++ app/routers/task_router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Form
+from datetime import datetime
 from app.services import task_service
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
@@ -6,20 +7,27 @@ router = APIRouter(prefix="/tasks", tags=["Tasks"])
 @router.post("/")
 async def create(
     title: str = Form(...),
-    description: str = Form(None),
+    description: str = Form(...),
     status: str = Form(...),
     priority: str = Form(...),
     estimated_hours: int = Form(...),
-    assigned_to: str = Form(None)
+    assigned_to: str = Form(None),
+    project_name: str = Form(...),
+    start_date: str = Form(None),
+    end_date: str = Form(None),
 ):
     data = {
-    "title": title,
-    "description": description,
-    "status": status,
-    "priority": priority,
-    "estimated_hours": estimated_hours,
-    "assigned_to": int(assigned_to) if assigned_to else None  # <-- fix this line
-}
+        "title": title,
+        "description": description,
+        "status": status,
+        "priority": priority,
+        "estimated_hours": estimated_hours,
+        "assigned_to": int(assigned_to) if assigned_to else None,
+        "project_name": project_name,
+       
+        "start_date": datetime.strptime(start_date, "%Y-%m-%d").date() if start_date else None,
+        "end_date":   datetime.strptime(end_date,   "%Y-%m-%d").date() if end_date   else None,
+    }
     return await task_service.create_task(data)
 
 
